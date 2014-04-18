c777a6a6deb205687275df81537c0bbc68c8ebb5
birkin <birkin.diana@gmail.com>
2014-04-18 10:13:29 -0400

    adds itg-staff login test; refactors code.

---
+++ home_page_login_tests.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import os, re, time, unittest
+from selenium import webdriver
+
+
+class HomePageLoginTest( unittest.TestCase ):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://library.brown.edu/"
+        # setup
+        self.USERNAME = os.environ.get( 'OCRA__LIBSTAFF_USERNAME' )
+        self.PASSWORD = os.environ.get( 'OCRA__LIBSTAFF_PASSWORD' )
+        # access reserves home page
+        self.driver.get(self.base_url + "/reserves/")
+
+    ##
+
+    def test_library_staff_login(self):
+        """ Tests Library Staff login.
+            Ensures login button brings up shib, then lands on correct page. """
+        driver = self.driver
+        # click 'Library Staff Login' button
+        driver.find_element_by_xpath("//input[@value='Library Staff Login']").click()
+        # test for Shib
+        self.assertTrue( u'sso.brown.edu' in driver.current_url )
+        # login
+        driver = self._log_into_shib( driver )
+        # test for lib-staff page
+        driver.find_element_by_css_selector("button[type=\"submit\"]").click()
+        self.assertTrue( u'reserves/staff/menu.php' in driver.current_url )
+
+    def test_itg_staff_login(self):
+        """ Tests ITG Staff login
+            Ensures login button brings up shib, then lands on correct page. """
+        driver = self.driver
+        # click 'Library Staff Login' button
+        driver.find_element_by_xpath("//input[@value='ITG Staff Login']").click()
+        # test for Shib
+        self.assertTrue( u'sso.brown.edu' in driver.current_url )
+        # login
+        driver = self._log_into_shib( driver )
+        # test for lib-staff page
+        driver.find_element_by_css_selector("button[type=\"submit\"]").click()
+        self.assertTrue( u'reserves/staff/menu_itg.php' in driver.current_url )
+
+    def _log_into_shib( self, driver ):
+        """ Helper function for tests.
+            Takes driver; logs in user; returns driver.
+            Called by module test functions. """
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys( self.USERNAME )
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys( self.PASSWORD )
+        return driver
+
+    ##
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+
+
+if __name__ == "__main__":
+    unittest.main( warnings='ignore' )  # warnings='ignore' from <http://stackoverflow.com/a/21500796>
