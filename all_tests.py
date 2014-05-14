3673cc036d5bfaa039b461163fe27ba02fbda4b2
birkin <birkin.diana@gmail.com>
2014-05-14 16:11:12 -0400

    finishes full book-chapter test.

--- all_tests.py
+++ all_tests.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+import datetime, unittest
 from faculty_add_article_via_details_test import FacultyAddArticleViaCitationTest
 from faculty_add_article_via_doi_test import FacultyAddArticleViaDoiTest
+from faculty_add_book_chapter_test import FacultyBookChapterTest
 from home_page_login_test import HomePageLoginTest
 
 
@@ -11,11 +12,14 @@ def make_suite():
     test_suite = unittest.TestSuite()
     test_suite.addTest( unittest.makeSuite(FacultyAddArticleViaCitationTest) )
     test_suite.addTest( unittest.makeSuite(FacultyAddArticleViaDoiTest) )
+    test_suite.addTest( unittest.makeSuite(FacultyBookChapterTest) )
     test_suite.addTest( unittest.makeSuite(HomePageLoginTest) )
     return test_suite
 
 full_suite = make_suite()
 
-
 runner=unittest.TextTestRunner( verbosity=2 )
+then = datetime.datetime.now()
 runner.run( full_suite )
+now = datetime.datetime.now()
+print( 'time-taken: %s' % str(now - then) )
